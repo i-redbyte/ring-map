96f5b100e7ea55d6311569f4b8320cfe4a86939f
Ilya Sokolov <developer.sokolov@gmail.com>
2022-04-27 00:53:06 +0300

    @fix: Corrected for the correct implementation. Non-thread-safe structure

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	v := NewVault(3)
-	v.Put("key1", "value1")
+	fmt.Printf("======> Start:")
+
+	v, err := NewVault(3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	v.Put("key1", "value1!")
 	fmt.Println(v.Len()) // 1
-	v.Put("key2", "value2")
+	v.Put("key2", "value2!")
 	fmt.Println(v.Len()) // 2
-	v.Put("key3", "value3")
+	v.Put("key3", "value3!")
 	fmt.Println(v.Len()) // 3
 	fmt.Println(v.Get("key1"))
-	v.Put("key4", "value4")
+	v.Put("key4", "value4!")
 	fmt.Println(v.Len())
 	fmt.Println(v.Keys())
+	fmt.Printf("======> End.")
 }
--- vault.go
+++ vault.go
@@ -1,58 +1,144 @@
 package main
 
+import (
+	"container/list"
+	"errors"
+)
+
 type Vault struct {
-	size        int
-	values      [][2]string
-	oldPosition int
-	position    int
+	size      int
+	evictList *list.List
+	items     map[interface{}]*list.Element
 }
 
-func NewVault(size int) *Vault {
-	rm := new(Vault)
-	rm.size = 0
-	rm.values = make([][2]string, size)
-	rm.position = 0
-	return rm
+type entry struct {
+	key   interface{}
+	value interface{}
 }
 
-func (v *Vault) Put(key, value string) {
-	if v.size < len(v.values) {
-		v.values[v.position][0] = key
-		v.values[v.position][1] = value
-		v.position = (v.position + 1) % len(v.values)
-	} else {
-		v.values[v.oldPosition][0] = key
-		v.values[v.oldPosition][1] = value
+func NewVault(size int) (*Vault, error) {
+	if size <= 0 {
+		return nil, errors.New("must provide a positive size")
 	}
-	if v.size < cap(v.values) {
-		v.size++
+	c := &Vault{
+		size:      size,
+		evictList: list.New(),
+		items:     make(map[interface{}]*list.Element),
 	}
+	return c, nil
 }
 
-func (v *Vault) Len() int {
-	return v.size
+func (c *Vault) Clear() {
+	for k, _ := range c.items {
+		delete(c.items, k)
+	}
+	c.evictList.Init()
 }
 
-func (v *Vault) Keys() []string {
-	var res []string
-	for i := 0; i < len(v.values); i++ {
-		if v.values[i][0] != "" {
-			res = append(res, v.values[i][0])
+func (c *Vault) Put(key, value interface{}) (evicted bool) {
+	if ent, ok := c.items[key]; ok {
+		c.evictList.MoveToFront(ent)
+		ent.Value.(*entry).value = value
+		return false
+	}
+
+	ent := &entry{key, value}
+	entry := c.evictList.PushFront(ent)
+	c.items[key] = entry
+
+	evict := c.evictList.Len() > c.size
+	if evict {
+		c.removeOldest()
+	}
+	return evict
+}
+
+func (c *Vault) Get(key interface{}) (value interface{}, ok bool) {
+	if ent, ok := c.items[key]; ok {
+		c.evictList.MoveToFront(ent)
+		if ent.Value.(*entry) == nil {
+			return nil, false
 		}
+		return ent.Value.(*entry).value, true
 	}
-	return res
+	return
 }
 
-func (v *Vault) Get(key string) string {
-	for i := 0; i < v.size; i++ {
-		if v.values[i][0] == key {
-			if i == len(v.values)-1 {
-				v.oldPosition = 0
-			} else {
-				v.oldPosition = i + 1
-			}
-			return v.values[i][1]
-		}
+func (c *Vault) Contains(key interface{}) (ok bool) {
+	_, ok = c.items[key]
+	return ok
+}
+
+func (c *Vault) Peek(key interface{}) (value interface{}, ok bool) {
+	var ent *list.Element
+	if ent, ok = c.items[key]; ok {
+		return ent.Value.(*entry).value, true
 	}
-	return ""
+	return nil, ok
+}
+
+func (c *Vault) Remove(key interface{}) (present bool) {
+	if ent, ok := c.items[key]; ok {
+		c.removeElement(ent)
+		return true
+	}
+	return false
+}
+
+func (c *Vault) RemoveOldest() (key, value interface{}, ok bool) {
+	ent := c.evictList.Back()
+	if ent != nil {
+		c.removeElement(ent)
+		kv := ent.Value.(*entry)
+		return kv.key, kv.value, true
+	}
+	return nil, nil, false
+}
+
+func (c *Vault) GetOldest() (key, value interface{}, ok bool) {
+	ent := c.evictList.Back()
+	if ent != nil {
+		kv := ent.Value.(*entry)
+		return kv.key, kv.value, true
+	}
+	return nil, nil, false
+}
+
+func (c *Vault) Keys() []interface{} {
+	keys := make([]interface{}, len(c.items))
+	i := 0
+	for ent := c.evictList.Back(); ent != nil; ent = ent.Prev() {
+		keys[i] = ent.Value.(*entry).key
+		i++
+	}
+	return keys
+}
+
+func (c *Vault) Len() int {
+	return c.evictList.Len()
+}
+
+func (c *Vault) Resize(size int) (evicted int) {
+	diff := c.Len() - size
+	if diff < 0 {
+		diff = 0
+	}
+	for i := 0; i < diff; i++ {
+		c.removeOldest()
+	}
+	c.size = size
+	return diff
+}
+
+func (c *Vault) removeOldest() {
+	ent := c.evictList.Back()
+	if ent != nil {
+		c.removeElement(ent)
+	}
+}
+
+func (c *Vault) removeElement(e *list.Element) {
+	c.evictList.Remove(e)
+	kv := e.Value.(*entry)
+	delete(c.items, kv.key)
 }
